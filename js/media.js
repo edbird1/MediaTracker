0f3b2ea44dad3710ac322e424d1241abc77cdad6
Elayna Bird <edbird1@yahoo.com>
2015-09-20 18:20:36 -0600

    Update media.js

--- js/media.js
+++ js/media.js
@@ -3,11 +3,11 @@ function showHide (name){
     obj = document.getElementById(name);
   
   if (obj.style.visibility == visible){
-      alert(obj.style.visibility+1);
+      alert(obj.style.visibility+ " if");
       obj.style.visibility = "hidden";
   }
   else {
-      alert(obj.style.visibility+2);
+      alert(obj.style.visibility+ " else");
       obj.style.visibility = "visible"; 
   }
 }
