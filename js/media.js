e1b7105031f273b3fde0080849221a26b8d30d3d
Bird <edbird1@yahoo.com>
2015-09-22 10:17:42 -0600

    Update JQuery and Toggle Forms

--- js/media.js
+++ js/media.js
@@ -2,13 +2,23 @@ $( document ).ready(function() {
 	console.log('have a pleasant day!');
 
 	$('.addDVD').on('click', function() {
-		alert('suck!!!');
+		alert('Yes!!!');
 		$('.mediaForm').toggle();
 	});
+
+	$(window).resize(sizeContent);
+
+	function sizeContent() {
+		var newHeight = $("html").height() - $("#header").height() - $("#footer").height() + "px";
+		$("#mediaInput").css("height", newHeight);
+	}
+		alert('Work!!!');
+
 });
 
 
-
+/*var mediaInputHeight = $("mediaInput").height();
+	$(".sidebar").height(mediaInputHeight);*/
 
 /*
 function showHide (name){
@@ -19,4 +29,4 @@ function showHide (name){
     } else {
         obj.style.visibility =  "visible"
     }
-}v */
+} */
