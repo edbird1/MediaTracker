d87f3b05cbbc2cf75847b8882def6aaa6a6b1758
Elayna Bird <edbird1@yahoo.com>
2015-09-26 09:53:12 -0600

    Update Angular in App

--- js/app.js
+++ js/app.js
@@ -1,5 +1,27 @@
 var myApp = angular.module('myApp', []);
 
-myApp.controller('mainController', function(){
+myApp.controller('mainController', function($scope){
+	$scope.list = []
 
 });
+
+var list = [
+	{
+		Name: "21", 
+		Media Type: "Blu-Ray", 
+		Genre: "Drama", 
+		Year: 2008, 
+		Length (min): 123, 
+		MPR (MPAA): "PG-13", 
+		CheckOut: false;
+	}, 
+	{
+		Name: "Baby Mama", 
+		Media Type: "DVD", 
+		Genre: "Comedy", 
+		Year: 2008, 
+		Length (min): 99, 
+		MPR (MPAA): "PG-13", 
+		CheckOut: false;
+	}
+]
