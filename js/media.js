5c24d9e140260c9e432a6e990e682fd5f0e68f0e
MATC <MATC>
2015-12-01 10:27:20 -0700

    Started Search Bar JS

--- js/media.js
+++ js/media.js
@@ -1,38 +1,41 @@
-$( document ).ready(function() {
-	console.log('have a pleasant day!');
+$(document).ready(function () {
+    console.log('have a pleasant day!');
 
-	$('.addDVD').on('click', function() {
-		console.log('Yes DVD!!!');
-		$('#formDVD').toggle();
-	});
+    $('.addDVD').on('click', function () {
+        console.log('Yes DVD!!!');
+        $('#formDVD').toggle();
+    });
 
-	$('.addCD').on('click', function() {
-		console.log('Yes CD!!!');
-		$('#formCD').toggle();
-	});
+    $('.addCD').on('click', function () {
+        console.log('Yes CD!!!');
+        $('#formCD').toggle();
+    });
 
-	$('.addBook').on('click', function() {
-		console.log('Yes Book!!!');
-		$('#formBook').toggle();
-	});
+    $('.addBook').on('click', function () {
+        console.log('Yes Book!!!');
+        $('#formBook').toggle();
+    });
 
-	function sizeContent() {
-		var docHeight = $(window).height();
-		console.log ('docHeight = ' + docHeight);
-		var headerHeight = $('#header').height();
-		console.log ('headerHeight = ' + headerHeight);
-		var footerHeight = $('#footer').height();
-		console.log ('footerHeight = ' + footerHeight);
-		var newHeight = docHeight - headerHeight - footerHeight - 2 + "px";
-		$("#sidebar").css("height", newHeight);
-	}
+    function sizeContent() {
+        var docHeight = $(window).height();
+        console.log('docHeight = ' + docHeight);
+        var headerHeight = $('#header').height();
+        console.log('headerHeight = ' + headerHeight);
+        var footerHeight = $('#footer').height();
+        console.log('footerHeight = ' + footerHeight);
+        var newHeight = docHeight - headerHeight - footerHeight - 2 + "px";
+        $("#sidebar").css("height", newHeight);
+    }
 
-	$(window).resize(sizeContent());
+    $(window).resize(sizeContent());
 
-	console.log('Work!!!');
+    console.log('Work!!!');
 
 });
 
+document.getElementById('frmSearch').onsubmit = function () {
+    window.location = 'http://www.google.com/search?q=site:http://edbird1.github.io/MediaTracker/ ' + document.getElementById('txtSearch').value;
+}
 
 /*var mediaInputHeight = $("mediaInput").height();
 	$(".sidebar").height(mediaInputHeight);*/
